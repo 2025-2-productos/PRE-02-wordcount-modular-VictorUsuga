b26c787120c3c6e1029404fe7472d5ea529734fe
VictorUsuga <vusuga@unal.edu.co>
2025-11-22 11:34:15 -0500

    intento 1

---
+++ homework/src/wordcount.py
@@ -0,0 +1,33 @@
+### wordcount.py
+# obtain a list of files in the input directory
+
+import sys
+
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
+from ._internals.write_word_counts import write_count_words
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Usage: python wordcount.py <input_folder> <output_folder>")
+        sys.exit(1)
+
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    all_lines = read_all_lines(input_folder)
+    all_lines = preprocess_lines(all_lines)
+    words = split_into_words(all_lines)
+    counter = count_words(words)
+    write_count_words(counter, output_folder)
+
+
+if __name__ == "__main__":
+    main()### wordcount.py
+# obtain a list of files in the input directory
+
+
